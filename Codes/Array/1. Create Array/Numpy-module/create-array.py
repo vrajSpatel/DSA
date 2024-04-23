8cfb6a6ea27ebc06cf0a1e8b0ef7b4eeda14de17
vrajSpatel <VSPATEL012004@GMAIL.COM>
2024-04-23 18:55:01 +0530

    Code

---
+++ Codes/Array/1. Create Array/Numpy-module/create-array.py
@@ -0,0 +1,12 @@
+''' 
+first you need to install numpy --> pip3 install numpy
+'''
+
+import numpy as np 
+
+arr1 = np.array([],dtype=int)
+print(arr1)
+
+# we dont need to give datatype like array module
+arr2 = np.array([1,1,2,3,4,5])
+print(arr2)
